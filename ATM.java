c71d2ae195806d7ae7385fde7a8986e444a740d6
omkarsachdeva <omkarsachdeva3832stella@gmail.com>
2023-10-13 22:22:35 +0530

    codsoft

---
+++ ATM.java
@@ -0,0 +1,85 @@
+import java.util.*;
+class Account{
+    double bal;
+
+    public Account(double init_bal){
+        bal=init_bal;
+    }
+    
+
+    public double Balance(){
+        return bal;
+    }
+
+    public void Deposit(double amt){
+        bal+=amt;
+    }
+
+    public boolean withdraw(double amt)
+    {
+        if(amt<=bal)
+        {
+            bal-=amt;
+            return true;
+        }
+        return false;
+    }
+}
+
+class Atm1
+{
+    Account acc;
+    public Atm1(Account Bankaccount){
+        acc=Bankaccount;
+    }
+    public void Menu(){
+        System.out.println("Atm Menu,choose an option");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit Amount");
+        System.out.println("3. Withdraw Amount");
+    }
+    public void transact(){
+        Scanner sc=new Scanner(System.in);
+        while(true){
+            Menu();
+            System.out.println("Select an option");
+            int c=sc.nextInt();
+            switch(c){
+                case 1:
+                System.out.println("Balance"+acc.Balance());
+                break;
+                case 2:
+                System.out.println("Enter amount to be deposited");
+                double dep_amount=sc.nextDouble();
+                acc.Deposit(dep_amount);
+                System.out.println("Updated Balance "+acc.Balance());
+                break;
+                case 3:
+                System.out.println("Enter amount to be withdrawn");
+                double with_amt=sc.nextDouble();
+                if(acc.withdraw(with_amt)) {
+                    System.out.println("Withdraw done.Updated Balance "+acc.Balance());
+                }
+                else{
+                    System.out.println("Insufficient Balance");
+
+                }
+                break;
+                default:
+                System.out.println("Invalid choice");
+                }
+            }
+        }
+    }
+        
+    
+
+public class ATM {
+    public static void main(String[]args)
+    {
+        Account userAccount=new Account(10000);
+       Atm1 atm1=new Atm1(userAccount);
+       atm1.transact(); 
+    }
+    
+}
